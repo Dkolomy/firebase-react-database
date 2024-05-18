56c5e4cfd0a999e95483bbc1c83a5b99e5ef6256
Dkolomy <Dkolomy@hotmail.com>
2024-05-18 16:50:10 -0400

    Inore key

--- src/services/db.mjs
+++ src/services/db.mjs
@@ -4,16 +4,16 @@ import { getFirestore } from "firebase/firestore";
 let db = false;
 
 export const getDb = () => {
-  if (!db) {
-    const firebaseConfig = {
-      apiKey: "AIzaSyBGVG-usA7gRalcm-ZgRgbhxqW1wPuITgs",
-      authDomain: "practicenew-6b0af.firebaseapp.com",
-      projectId: "practicenew-6b0af",
-      storageBucket: "practicenew-6b0af.appspot.com",
-      messagingSenderId: "574945103525",
-      appId: "1:574945103525:web:a4ca1ddb328c4fe540874c",
-      measurementId: "G-3F8RTT3MS6",
-    };
+  // if (!db) {
+  //   const firebaseConfig = {
+  //     apiKey: "AIzaSyBGVG-usA7gRalcm-ZgRgbhxqW1wPuITgs",
+  //     authDomain: "practicenew-6b0af.firebaseapp.com",
+  //     projectId: "practicenew-6b0af",
+  //     storageBucket: "practicenew-6b0af.appspot.com",
+  //     messagingSenderId: "574945103525",
+  //     appId: "1:574945103525:web:a4ca1ddb328c4fe540874c",
+  //     measurementId: "G-3F8RTT3MS6",
+  //   };
 
     const app = initializeApp(firebaseConfig);
 
