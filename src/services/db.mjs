9118ebd0823c6817fb98e19b3d8feca1fcc3612a
Dkolomy <Dkolomy@hotmail.com>
2024-05-18 20:38:53 -0400

    Update security

--- src/services/db.mjs
+++ src/services/db.mjs
@@ -4,16 +4,16 @@ import { getFirestore } from "firebase/firestore";
 let db = false;
 
 export const getDb = () => {
-  // if (!db) {
-  //   const firebaseConfig = {
-  //     apiKey: "AIzaSyBGVG-usA7gRalcm-ZgRgbhxqW1wPuITgs",
-  //     authDomain: "practicenew-6b0af.firebaseapp.com",
-  //     projectId: "practicenew-6b0af",
-  //     storageBucket: "practicenew-6b0af.appspot.com",
-  //     messagingSenderId: "574945103525",
-  //     appId: "1:574945103525:web:a4ca1ddb328c4fe540874c",
-  //     measurementId: "G-3F8RTT3MS6",
-  //   };
+  if (!db) {
+    const firebaseConfig = {
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_APP_ID,
+      measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+    };
 
     const app = initializeApp(firebaseConfig);
 
